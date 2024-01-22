1523a1b90b85afd2acd9ef415c80e1d7782acb2b
Bulelani Gabonewe <53180071+gaboneweb@users.noreply.github.com>
2024-01-22 17:07:41 +0200

    Update nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,7 +1,7 @@
 const navToggle = () => {
     const menu = document.querySelector('.button-toogler');
     const nav = document.querySelector('.navbar-links-dropdown');
-    console.log("Button has been clicked");
+    
 
     menu.addEventListener('click', () => {
         nav.classList.toggle('navbar-links-dropdown-active');
